232e79a8595d9abc8a91784392b7689808a32a61
Nkasi-e <nkasijnr@gmail.com>
2023-01-27 23:06:19 +0100

    upgraded the database query from raw sql to python code using slqalchemy as my orm and this increased the efficiency of the api as I also defined the pydantic model for response and request

--- app/main.py
+++ app/main.py
@@ -1,51 +1,14 @@
-import os
-from typing import Optional
-from fastapi import FastAPI, HTTPException, status, Response
-from pydantic import BaseModel
-from random import randrange
-import psycopg2
-from psycopg2.extras import RealDictCursor
-from dotenv import load_dotenv
-import time
+from typing import List
+from fastapi import FastAPI, HTTPException, Depends
+from . import models
+from .config import engine, get_db
+from sqlalchemy.orm import Session
+from .schemas import CreatePost, Post
 
-load_dotenv()
 
-# working_directory = os.getcwd()
-# env = os.path.join(working_directory, '.env')
-# # loading evn
-# if os.path.exists(env):
-#     load_dotenv(env)
-#     print('env loaded successfully')
-# else:
-#     print('could not find environment')
+models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
-
-
-class Post(BaseModel):
-    id: Optional[int]
-    title: str
-    content: str
-    published: Optional[bool] = True
-
-
-class UpdatePost(BaseModel):
-    id: Optional[int]
-    title: Optional[str]
-    content: Optional[str]
-    published: Optional[bool] = True
-
-
-while True:
-    try:
-        conn = psycopg2.connect(host=os.getenv('DB_HOST'), database=os.environ.get(
-            'USER_DB'), user='postgres', password=os.environ.get('DB_PASSWORD'), cursor_factory=RealDictCursor)
-        cursor = conn.cursor()
-        print('Database connection established')
-        break
-    except Exception as error:
-        print(f'Connection to database failed {error}')
-        time.sleep(3)
 
 
 @app.get("/")
@@ -53,76 +16,56 @@ async def root():
     return {"message": "Welcome to my API"}
 
 
-@app.get('/posts')
-async def get_posts():
-    cursor.execute('''SELECT * FROM posts''')
-    db_post = cursor.fetchall()
-    return {"My Posts": db_post, "total": len(db_post)}
+@app.get('/posts', response_model=List[Post])
+async def get_posts(db: Session = Depends(get_db)):
+    posts = db.query(models.Post).all()
+    return posts
 
 
-@app.get('/posts/latest')
-async def get_latest_posts():
-    cursor.execute('''SELECT * FROM posts''')
-    db_post = cursor.fetchall()
-    post = db_post[len(db_post)-1]
-    return {"latest Post": post}
+@app.post('/posts', status_code=201, response_model=Post)
+async def create_post(post: CreatePost, db: Session = Depends(get_db)):
+    new_post = models.Post(**post.dict())
+    db.add(new_post)
+    db.commit()
+    db.refresh(new_post)
+    return new_post
 
 
-@app.post('/posts', status_code=201)
-async def create_post(post: Post):
-    cursor.execute("""INSERT INTO posts (title, content, published) VALUES (%s, %s, %s) RETURNING * """,
-                   (post.title, post.content, post.published))
-    new_post = cursor.fetchone()
-    conn.commit()
-    return {"data": new_post}
-
-
-@app.get('/posts/{id}')
-async def get_posts(id: int):
-    cursor.execute('''SELECT * FROM posts WHERE id = %s ''', (str(id)))
-    post = cursor.fetchone()
+@app.get('/posts/{id}', response_model=Post)
+async def get_posts(id: int, db: Session = Depends(get_db)):
+    post = db.query(models.Post).filter(models.Post.id == id).first()
     if not post:
         raise HTTPException(
             status_code=404,
             detail=f'Post with id {id} not found'
         )
-    return {"Post detail": post}
+    return post
 
 
 @app.delete('/posts/{id}', status_code=204)
-async def delete_post(id: int):
-    cursor.execute(
-        '''DELETE FROM posts WHERE id = %s RETURNING * ''', (str(id)))
-    deleted_post = cursor.fetchone()
-    conn.commit()
-    if deleted_post is None:
+async def delete_post(id: int, db: Session = Depends(get_db)):
+    post = db.query(models.Post).filter(models.Post.id == id)
+    if post.first() is None:
         raise HTTPException(
             status_code=404,
             detail=f'Post with id {id} not found')
+    post.delete(synchronize_session=False)
+    db.commit()
     return
 
 
-@app.put('/posts/{id}')
-async def update_post(id: int, post: UpdatePost):
-    cursor.execute('''UPDATE posts SET content = %s, published = %s WHERE id = %s RETURNING *''',
-                   (post.content, post.published, str(id)))
-    updated_post = cursor.fetchone()
-    conn.commit()
-    if updated_post is None:
+@app.put('/posts/{id}', response_model=Post)
+async def update_post(id: int, updated_post: CreatePost, db: Session = Depends(get_db)):
+    post_query = db.query(models.Post).filter(models.Post.id == id)
+    post = post_query.first()
+    if post == None:
         raise HTTPException(
             status_code=404,
-            detail=f'Post with id {id} not found')
-    return {"data": updated_post}
-
-
-# @app.patch('/posts/{id}')
-# async def update_post(id: int, post: UpdatePost):
-#     index = find_index_post(id)
-#     if index is None:
-#         raise HTTPException(
-#             status_code=404,
-#             detail=f'Post with id {id} not found')
-#     post_dict = post.dict()
-#     post_dict['id'] = id
-#     posts[index] = post_dict
-#     return {"message": f'Post with id {id} successfully updated', "data": post_dict}
+            detail=f'Post with id {id} does not exists'
+        )
+    update_data = updated_post.dict(exclude_unset=True)
+    post_query.filter(models.Post.id == id).update(
+        update_data, synchronize_session=False)
+    db.commit()
+    db.refresh(post)
+    return post
---
+++ app/models.py
@@ -0,0 +1,15 @@
+from .config import Base
+from sqlalchemy import Column, Integer, String, Boolean
+from sqlalchemy.sql.sqltypes import TIMESTAMP
+from sqlalchemy.sql.expression import text
+
+
+class Post(Base):
+    __tablename__ = 'posts'
+
+    id = Column(Integer, primary_key=True, autoincrement=True, nullable=False)
+    title = Column(String, nullable=False)
+    content = Column(String(1000), nullable=False)
+    published = Column(Boolean, server_default='FALSE', nullable=False)
+    created_at = Column(TIMESTAMP(timezone=True),
+                        nullable=False, server_default=text('now()'))
